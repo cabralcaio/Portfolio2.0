52a699a36f832fc6930ec7bdc0a4492e53b0452b
cabralcaio <caio_cabral1@hotmail.com>
2023-01-10 23:04:43 -0300

    Seção -meu intuito- implementada

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +6,53 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  counterAnimation(n1:number, id: string) {
+    let time = 50
+    let upto=0;
+    if(id == "counter3"){
+      time = 100
+    }
 
+    let counts=setInterval(updated,time);
+        function updated(){
+            let count: any= document.getElementById(id);
+            if(id == "counter3") {
+              count.innerHTML=`${++upto}mil`;
+              if(upto===n1 || upto > 51)
+            {
+                clearInterval(counts);
+            }
+            } else {
+              count.innerHTML=`${++upto}%`;
+              if(upto===n1 || upto > 99)
+            {
+                clearInterval(counts);
+            }
+            }
+            
+        }
+
+  }
+
+  
+  @ViewChild('counter')myElement!: ElementRef;
+
+  ngAfterViewInit() {
+    const observer = new IntersectionObserver((entries) => {
+      if (entries[0].isIntersecting) {
+        // Código a ser executado quando o elemento for exibido na tela
+        this.counterAnimation(97, "counter")
+        console.log("view element")
+        this.counterAnimation(51428, "counter3")
+        this.counterAnimation(81, "counter2");
+      }
+    });
+    observer.observe(this.myElement.nativeElement);
+  }
+
+  timer() {
+    setTimeout(() => {
+      
+    }, 3000);
+  }
 }
