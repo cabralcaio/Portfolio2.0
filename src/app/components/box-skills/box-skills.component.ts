2d56569b84150afefc0da93ea39e43f2e943e025
cabralcaio <caio_cabral1@hotmail.com>
2023-01-11 00:14:59 -0300

    Lógica das skilss implementada, portfólio finalizado

--- src/app/components/box-skills/box-skills.component.ts
+++ src/app/components/box-skills/box-skills.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./box-skills.component.css']
 })
 export class BoxSkillsComponent {
+  public itemMain: boolean = true
+  public itemHtml:boolean = false
+  public itemJava:boolean = false
+  public itemJs:boolean = false
+  public itemAngular:boolean = false
+
+  ocultar (number: number){
+    if(number == 1){ this.itemHtml = true } 
+    if(number == 2){ this.itemJava = true } 
+    if(number == 3){ this.itemJs = true } 
+    if(number == 4){ this.itemAngular = true } 
+    this.itemMain = false
+  }
+
+  mostrar () {
+    this.itemMain = true
+    this.itemAngular = false
+    this.itemHtml = false
+    this.itemJava = false
+    this.itemJs = false
+  }
 
 }
